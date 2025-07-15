63ef307422646839cb76ef32489c743139640d76
Sandeep Kadyan <sandeepkadyan91@gmail.com>
2025-07-15 20:32:15 +0530

    Fix return type from the command

--- src/Command/MaintenanceCommand.php
+++ src/Command/MaintenanceCommand.php
@@ -51,9 +51,9 @@ class MaintenanceCommand extends Command
      *
      * @param \Cake\Console\Arguments $args The command arguments.
      * @param \Cake\Console\ConsoleIo $io The console io
-     * @return int|null|void The exit code or null for success
+     * @return int|null The exit code or null for success
      */
-    public function execute(Arguments $args, ConsoleIo $io): int|null|null
+    public function execute(Arguments $args, ConsoleIo $io): int|null
     {
         $action = $args->getArgument('action');
 
